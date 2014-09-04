886bfe6f9b7f8c8c2e26af0167bd8892df0f64d5
Namide <damien.git@gmail.com>
2014-09-04 15:38:51 +0200

    Initialization

---
+++ executed-scripts/time-remapper.jsx
@@ -0,0 +1,228 @@
+﻿/*
+	  __  _                                    
+	 / /_(_)_ _  ___                           
+	/ __/ /  ' \/ -_)                          
+	\__/_/_/_/_/\__/______   ___  ___  _______ 
+	  / _ \/ __/  |/  / _ | / _ \/ _ \/ __/ _ \
+	 / , _/ _// /|_/ / __ |/ ___/ ___/ _// , _/
+	/_/|_/___/_/  /_/_/ |_/_/  /_/  /___/_/|_| 
+	
+			  By Damien Doussaud - namide.com
+	
+	
+    Usage: 
+	
+		Select layers in AE. 
+		Select File -> Script -> Run script... from the menu and run this script.
+
+		
+    Tags (adds to layer's name) :
+	
+		.unlinked (no time)
+		.t[x] (time in milliseconds, example .t6000 = 6 seconds)
+		.noTransition (unactive alpha and transition time)
+
+		.up (bottom to top transition)
+		.down (top to bottom transition)
+		.right (left to right transition)
+		.left (right to left transition)
+		.back (front to back transition)
+		.front (back to front transition)
+		//.alpha (alpha transition [default])
+    
+ */
+
+function hasTag( layer, tag )
+{
+    var n = layer.name;
+    var a = n.split(".");
+    
+    for ( var i = 0; i < a.length; i++ )
+    {
+        if (    tag == "time" &&
+                a[i].charAt(0) == "t" &&
+                a[i].substr(1) == String(Number( a[i].substr(1) )) )
+        {
+            return true;
+        }
+        if ( a[i] == tag ) return true;
+    }
+    return false;
+}
+
+function getTimeByTag( layer )
+{
+    var n = layer.name;
+    var a = n.split(".");
+    for ( var i = 0; i < a.length; i++ )
+    {
+        if ( a[i].charAt(0) == "t" )
+        {
+            var n = Number( a[i].substr(1) );
+            n /= 1000;
+            if ( n == null || n == NaN || n < 0 || n >= Infinity ) n = 1;
+            return n;
+         }
+    }
+    return 1;
+}
+
+function remap( list )
+{
+    var d = 0;
+    for ( var i = list.length-1; i > -1; i-- )
+    {
+        var layer = list[i];
+        var tTrans = 0;
+    
+        var t = 0;
+        if ( layer.matchName == "ADBE AV Layer" )
+        {
+            var lc = layer.source.layers;
+            var list2 = [];
+            for ( var j = 1; j <= lc.length ; j++ )
+            {
+                list2.push(lc[j]);
+            }
+            t = remap( list2 );
+            layer.source.duration = t;
+        }
+        else if ( layer.matchName == "ADBE Text Layer" )
+        {
+            var carPerS = 25;
+            t = layer.property("sourceText").value.text.length / carPerS;
+            if ( t < 1 ) t = 1;
+            if ( hasTag( layer, "time" ) ) t = getTimeByTag(layer);    
+            if ( !hasTag( layer, "noTransition" ) ) tTrans = addTween(layer, "alpha");
+        }
+        else
+        {
+            t = 3;
+            if ( hasTag( layer, "time" ) ) t = getTimeByTag(layer);    
+            if ( !hasTag( layer, "noTransition" ) ) tTrans = addTween(layer, "alpha");
+        }
+        
+        if ( hasTag( layer, "up" ) ) tTrans = addTween( layer, "up" );
+        if ( hasTag( layer, "down" ) ) tTrans = addTween( layer, "down" );
+        if ( hasTag( layer, "right" ) ) tTrans = addTween( layer, "right" );
+        if ( hasTag( layer, "left" ) ) tTrans = addTween( layer, "left" );
+        if ( hasTag( layer, "back" ) ) tTrans = addTween( layer, "back" );
+        if ( hasTag( layer, "front" ) ) tTrans = addTween( layer, "front" );
+        if ( hasTag( layer, "back" ) ) tTrans = addTween( layer, "back" );     
+        
+        t += tTrans;
+        
+        layer.startTime = d;
+        layer.outPoint = t + d;
+        if ( !hasTag( layer, "unlinked" ) ) d += t;
+    }
+    
+    return d;
+}
+
+function startRemmaping()
+{
+    var layers = app.project.activeItem.selectedLayers;
+    var t = remap( layers );
+    app.project.activeItem.workAreaDuration = t;
+    app.project.activeItem.duration = t + 1;
+    return t;
+}
+
+function addTween( layer, tween )
+{
+    if ( tween == "alpha" )
+    {
+        var p = layer.transform[ "Opacity" ];
+        p.expressionEnabled = true;
+        
+        var script = "var o = 100;\n";
+        script += "if ( time < inPoint + 1 ) { o = linear(time, inPoint, inPoint + 1, 0, 100); }\n";
+        script += "else if ( time > outPoint - 1 ) { o = linear(time, outPoint - 1, outPoint, 100, 0); }\n";
+        script += "o;\n";
+        p.expression = script;
+    }
+    if ( tween == "down" )
+    {
+        var p = layer.transform[ "Position" ];
+        p.expressionEnabled = true;
+        
+        var script = "var p = [transform.position[0], transform.position[1]];\n";
+        script += "if ( time < inPoint + 1 ) { p[1] = easeOutExpo( time-inPoint, p[1] - 1080, 1080, 1 ); }\n";
+        script += "else if ( time > outPoint - 1 ) { p[1] = easeInExpo( time-(outPoint-1), p[1], 1080, 1 ); }\n";
+        script += "p;\n";
+        script += "function easeInExpo(t, b, c, d) { return(t==0) ? b : c * Math.pow(2, 10 *(t/d - 1)) + b - c * 0.001; }\n";
+        script += "function easeOutExpo(t, b, c, d) { return(t==d) ? b+c : c * 1.001 *(-Math.pow(2, -10 * t/d) + 1) + b; }\n";
+        p.expression = script;
+    }
+    if ( tween == "up" )
+    {
+        var p = layer.transform[ "Position" ];
+        p.expressionEnabled = true;
+        
+        var script = "var p = [transform.position[0], transform.position[1]];\n";
+        script += "if ( time < inPoint + 1 ) { p[1] = easeOutExpo( time-inPoint, p[1] + 1080, -1080, 1 ); }\n";
+        script += "else if ( time > outPoint - 1 ) { p[1] = easeInExpo( time-(outPoint-1), p[1], -1080, 1 ); }\n";
+        script += "p;\n";
+        script += "function easeInExpo(t, b, c, d) { return(t==0) ? b : c * Math.pow(2, 10 *(t/d - 1)) + b - c * 0.001; }\n";
+        script += "function easeOutExpo(t, b, c, d) { return(t==d) ? b+c : c * 1.001 *(-Math.pow(2, -10 * t/d) + 1) + b; }\n";
+        p.expression = script;
+    }
+    if ( tween == "left" )
+    {
+        var p = layer.transform[ "Position" ];
+        p.expressionEnabled = true;
+        
+        var script = "var p = [transform.position[0], transform.position[1]];\n";
+        script += "if ( time < inPoint + 1 ) { p[0] = easeOutExpo( time-inPoint, p[0] + 1920, -1920, 1 ); }\n";
+        script += "else if ( time > outPoint - 1 ) { p[0] = easeInExpo( time-(outPoint-1), p[0], -1920, 1 ); }\n";
+        script += "p;\n";
+        script += "function easeInExpo(t, b, c, d) { return(t==0) ? b : c * Math.pow(2, 10 *(t/d - 1)) + b - c * 0.001; }\n";
+        script += "function easeOutExpo(t, b, c, d) { return(t==d) ? b+c : c * 1.001 *(-Math.pow(2, -10 * t/d) + 1) + b; }\n";
+        p.expression = script;
+    }
+    if ( tween == "right" )
+    {
+        var p = layer.transform[ "Position" ];
+        p.expressionEnabled = true;
+        
+        var script = "var p = [transform.position[0], transform.position[1]];\n";
+        script += "if ( time < inPoint + 1 ) { p[0] = easeOutExpo( time-inPoint, p[0] - 1920, 1920, 1 ); }\n";
+        script += "else if ( time > outPoint - 1 ) { p[0] = easeInExpo( time-(outPoint-1), p[0], 1920, 1 ); }\n";
+        script += "p;\n";
+        script += "function easeInExpo(t, b, c, d) { return(t==0) ? b : c * Math.pow(2, 10 *(t/d - 1)) + b - c * 0.001; }\n";
+        script += "function easeOutExpo(t, b, c, d) { return(t==d) ? b+c : c * 1.001 *(-Math.pow(2, -10 * t/d) + 1) + b; }\n";
+        p.expression = script;
+    }
+    if ( tween == "back" )
+    {
+        layer.threeDLayer = true;
+        var p = layer.transform[ "Position" ];
+        p.expressionEnabled = true;
+        
+        var script = "var p = [transform.position[0], transform.position[1], transform.position[2]];\n";
+        script += "if ( time < inPoint + 1 ) { p[2] = easeOutExpo( time-inPoint, p[2] - 2048, 2048, 1 ); }\n";
+        script += "else if ( time > outPoint - 1 ) { p[2] = easeInExpo( time-(outPoint-1), p[2], 16000, 1 ); }\n";
+        script += "p;\n";
+        script += "function easeInExpo(t, b, c, d) { return(t==0) ? b : c * Math.pow(2, 10 *(t/d - 1)) + b - c * 0.001; }\n";
+        script += "function easeOutExpo(t, b, c, d) { return(t==d) ? b+c : c * 1.001 *(-Math.pow(2, -10 * t/d) + 1) + b; }\n";
+        p.expression = script;
+    }
+    if ( tween == "front" )
+    {
+        layer.threeDLayer = true;
+        var p = layer.transform[ "Position" ];
+        p.expressionEnabled = true;
+        
+        var script = "var p = [transform.position[0], transform.position[1], transform.position[2]];\n";
+        script += "if ( time < inPoint + 1 ) { p[2] = easeOutExpo( time-inPoint, p[2] + 16000, -16000, 1 ); }\n";
+        script += "else if ( time > outPoint - 1 ) { p[2] = easeInExpo( time-(outPoint-1), p[2], -2048, 1 ); }\n";
+        script += "p;\n";
+        script += "function easeInExpo(t, b, c, d) { return(t==0) ? b : c * Math.pow(2, 10 *(t/d - 1)) + b - c * 0.001; }\n";
+        script += "function easeOutExpo(t, b, c, d) { return(t==d) ? b+c : c * 1.001 *(-Math.pow(2, -10 * t/d) + 1) + b; }\n";
+        p.expression = script;
+    }
+    return 2;
+}
+
+startRemmaping();
---
+++ properties-scripts/tweens.js
@@ -0,0 +1,348 @@
+
+var introTime = 1;
+var introDatas = [ 0, 0, 0 ];
+var introTween = easeOutQuint;
+
+var endTime = 1;
+var endDatas = [ 0, 0, 0 ];
+var endTween = easeInQuint;
+
+var dontStop = false;
+var relative = false;
+
+
+
+/*
+
+	CALCULATION
+
+*/
+
+var newDatas = [];
+var depth = ( thisProperty.value == Number(thisProperty.value) ) ? 1 : thisProperty.value.length;
+var datas = ( depth < 2 ) ? new Array(thisProperty) : thisProperty.value;
+
+var i;
+if( dontStop )
+{
+	var totalTime = outPoint - inPoint;
+	var ratio = introTime / ( introTime + endTime );
+	introTime = totalTime * ratio;
+	endTime = totalTime * ( 1 - ratio );
+}
+if( relative )
+{
+	for ( i = 0; i < depth; i++ )
+	{
+		 introDatas[i] += datas[i];
+		 endDatas[i] += datas[i];
+	}
+}
+
+
+if ( time < inPoint  )
+{
+	for ( i = 0; i < depth; i++ ) newDatas[i] = introDatas[i];
+}
+else if ( time < inPoint + introTime )
+{
+	for ( i = 0; i < depth; i++ ) newDatas[i] = introTween( time-inPoint, introDatas[i], datas[i]-introDatas[i], introTime );
+}
+else if( time > outPoint )
+{
+	for ( i = 0; i < depth; i++ ) newDatas[i] = endDatas[i];
+}
+else if ( time > outPoint - endTime )
+{
+	for ( i = 0; i < depth; i++ ) newDatas[i] = endTween( time + endTime - outPoint, datas[i], endDatas[i]-datas[i], endTime );
+}
+else
+{
+	for ( i = 0; i < depth; i++ ) newDatas[i] = datas[i];
+}
+
+if ( depth < 2 ) newDatas[0];
+else newDatas;
+
+
+/*
+
+
+	List of transitions:
+
+		easeNone
+		easeInQuad
+		easeInOutQuad
+		easeInCubic
+		easeOutCubic
+		easeInOutCubic
+		easeOutInCubic
+		easeInQuart
+		easeOutQuart
+		easeInOutQuart
+		easeOutInQuart
+		easeOutQuart
+		easeInQuint
+		easeOutQuint
+		easeInOutQuint
+		easeOutInQuint
+		easeInSine
+		easeOutSine
+		easeInOutSine
+		easeOutInSine
+		easeInExpo
+		easeOutExpo
+		easeInOutExpo
+		easeOutInExpo
+		easeInCirc
+		easeOutCirc
+		easeInOutCirc
+		easeOutInCirc
+		easeInElastic
+		easeOutElastic
+		easeInOutElastic
+		easeOutInElastic
+		easeInCushion
+		easeOutCushion
+		easeInOutCushion
+		easeInBack
+		easeOutBack
+		easeInOutBack
+		easeOutInBack
+		easeInBounce
+		easeOutBounce
+		easeInOutBounce
+		easeOutInBounce
+
+	
+	Parameters of transitions:
+
+		t - current time of tween
+		b - starting value of property
+		c - change needed in value
+		d - total duration of tween
+
+
+*/
+function easeNone(t, b, c, d)
+{
+	return c*t/d + b;
+}  
+function easeInQuad(t, b, c, d)
+{
+	return c*(t/=d)*t + b;
+}
+function easeOutQuad(t, b, c, d)
+{
+	return -c *(t/=d)*(t-2) + b;
+} 
+function easeInOutQuad(t, b, c, d)
+{
+	if((t/=d/2) < 1) return c/2*t*t + b;
+	return -c/2 *((--t)*(t-2) - 1) + b;
+}  
+function easeInCubic(t, b, c, d)
+{
+	return c*(t/=d)*t*t + b;
+}   
+function easeOutCubic(t, b, c, d)
+{
+	return c*((t=t/d-1)*t*t + 1) + b;
+}   
+function easeInOutCubic(t, b, c, d)
+{
+	if((t/=d/2) < 1) return c/2*t*t*t + b;
+	return c/2*((t-=2)*t*t + 2) + b;
+}   
+function easeOutInCubic(t, b, c, d)
+{
+	if(t < d/2) return easeOutCubic(t*2, b, c/2, d);
+	return easeInCubic((t*2)-d, b+c/2, c/2, d);
+}
+function easeInQuart(t, b, c, d)
+{
+	return c*(t/=d)*t*t*t + b;
+}  
+function easeOutQuart(t, b, c, d)
+{
+	return -c *((t=t/d-1)*t*t*t - 1) + b;
+}   
+function easeInOutQuart(t, b, c, d)
+{
+	if((t/=d/2) < 1) return c/2*t*t*t*t + b;
+	return -c/2 *((t-=2)*t*t*t - 2) + b;
+}    
+function easeOutInQuart(t, b, c, d)
+{
+	if(t < d/2) return easeOutQuart(t*2, b, c/2, d);
+	return easeInQuart((t*2)-d, b+c/2, c/2, d);
+} 
+function easeInQuint(t, b, c, d)
+{
+	return c*(t/=d)*t*t*t*t + b;
+}    
+function easeOutQuint(t, b, c, d)
+{
+	return c*((t=t/d-1)*t*t*t*t + 1) + b;
+}    
+function easeInOutQuint(t, b, c, d)
+{
+	if((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+	return c/2*((t-=2)*t*t*t*t + 2) + b;
+}    
+function easeOutInQuint(t, b, c, d)
+{
+	if(t < d/2) return easeOutQuint(t*2, b, c/2, d);
+	return easeInQuint((t*2)-d, b+c/2, c/2, d);
+}    
+function easeInSine(t, b, c, d)
+{
+	return -c * Math.cos(t/d *(Math.PI/2)) + c + b;
+}    
+function easeOutSine(t, b, c, d)
+{
+	return c * Math.sin(t/d *(Math.PI/2)) + b;
+}    
+function easeInOutSine(t, b, c, d)
+{
+	return -c/2 *(Math.cos(Math.PI*t/d) - 1) + b;
+}    
+function easeOutInSine(t, b, c, d)
+{
+	if(t < d/2) return easeOutSine(t*2, b, c/2, d);
+	return easeInSine((t*2)-d, b+c/2, c/2, d);
+}    
+function easeInExpo(t, b, c, d)
+{
+	return(t==0) ? b : c * Math.pow(2, 10 *(t/d - 1)) + b - c * 0.001;
+}    
+function easeOutExpo(t, b, c, d)
+{
+	return(t==d) ? b+c : c * 1.001 *(-Math.pow(2, -10 * t/d) + 1) + b;
+}    
+function easeInOutExpo(t, b, c, d)
+{
+	if(t==0) return b;
+	if(t==d) return b+c;
+	if((t/=d/2) < 1) return c/2 * Math.pow(2, 10 *(t - 1)) + b - c * 0.0005;
+	return c/2 * 1.0005 *(-Math.pow(2, -10 * --t) + 2) + b;
+}    
+function easeOutInExpo(t, b, c, d)
+{
+	if(t < d/2) return easeOutExpo(t*2, b, c/2, d);
+	return easeInExpo((t*2)-d, b+c/2, c/2, d);
+}    
+function easeInCirc(t, b, c, d)
+{
+	return -c *(Math.sqrt(1 -(t/=d)*t) - 1) + b;
+}    
+function easeOutCirc(t, b, c, d)
+{
+	return c * Math.sqrt(1 -(t=t/d-1)*t) + b;
+}    
+function easeInOutCirc(t, b, c, d)
+{
+	if((t/=d/2) < 1) return -c/2 *(Math.sqrt(1 - t*t) - 1) + b;
+	return c/2 *(Math.sqrt(1 -(t-=2)*t) + 1) + b;
+}    
+function easeOutInCirc(t, b, c, d)
+{
+	if(t < d/2) return easeOutCirc(t*2, b, c/2, d);
+	return easeInCirc((t*2)-d, b+c/2, c/2, d);
+}    
+function easeInElastic(t, b, c, d, a, p)
+{
+	var s;
+	if(t==0) return b;  if((t/=d)==1) return b+c;  if(!p) p=d*.3;
+	if(!a || a < Math.abs(c)) { a=c; s=p/4; } else s = p/(2*Math.PI) * Math.asin(c/a);
+	return -(a*Math.pow(2,10*(t-=1)) * Math.sin((t*d-s)*(2*Math.PI)/p )) + b;
+}    
+function easeOutElastic(t, b, c, d, a, p)
+{
+	var s;
+	if(t==0) return b;  if((t/=d)==1) return b+c;  if(!p) p=d*.3;
+	if(!a || a < Math.abs(c)) { a=c; s=p/4; } else s = p/(2*Math.PI) * Math.asin(c/a);
+	return(a*Math.pow(2,-10*t) * Math.sin((t*d-s)*(2*Math.PI)/p ) + c + b);
+}    
+function easeInOutElastic(t, b, c, d, a, p)
+{
+	var s;
+	if(t==0) return b;  if((t/=d/2)==2) return b+c;  if(!p) p=d*(.3*1.5);
+	if(!a || a < Math.abs(c)) { a=c; s=p/4; }       else s = p/(2*Math.PI) * Math.asin(c/a);
+	if(t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin((t*d-s)*(2*Math.PI)/p )) + b;
+	return a*Math.pow(2,-10*(t-=1)) * Math.sin((t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+}    
+function easeOutInElastic(t, b, c, d, a, p)
+{
+	if(t < d/2) return easeOutElastic(t*2, b, c/2, d, a, p);
+	return easeInElastic((t*2)-d, b+c/2, c/2, d, a, p);
+}
+function easeInCushion(t, b, c, d, a, p)
+{
+	var s;
+	if(t==0) return b;  if((t/=d)==1) return b+c;  if(!p) p=d*.7;
+	if(!a || a < Math.abs(c)) { a=c; s=p/4; } else s = p/(2*Math.PI) * Math.asin(c/a);
+	return -(a*Math.pow(2,10*(t-=1)) * Math.sin((t*d-s)*(2*Math.PI)/p )) + b;
+} 
+function easeOutCushion(t, b, c, d, a, p)
+{
+	var s;
+	if(t==0) return b;  if((t/=d)==1) return b+c;  if(!p) p=d*.7;
+	if(!a || a < Math.abs(c)) { a=c; s=p/4; } else s = p/(2*Math.PI) * Math.asin(c/a);
+	return(a*Math.pow(2,-10*t) * Math.sin((t*d-s)*(2*Math.PI)/p ) + c + b);
+} 
+function easeInOutCushion(t, b, c, d, a, p)
+{
+	var s;
+	if(t==0) return b;  if((t/=d/2)==2) return b+c;  if(!p) p=d*(.7*1.5);
+	if(!a || a < Math.abs(c)) { a=c; s=p/4; }       else s = p/(2*Math.PI) * Math.asin(c/a);
+	if(t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin((t*d-s)*(2*Math.PI)/p )) + b;
+	return a*Math.pow(2,-10*(t-=1)) * Math.sin((t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+}
+function easeInBack(t, b, c, d, s)
+{
+	if(s == undefined) s = 1.70158;
+	return c*(t/=d)*t*((s+1)*t - s) + b;
+}    
+function easeOutBack(t, b, c, d, s)
+{
+	if(s == undefined) s = 1.70158;
+	return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+}    
+function easeInOutBack(t, b, c, d, s)
+{
+	if(s == undefined) s = 1.70158;
+	if((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+	return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+}    
+function easeOutInBack(t, b, c, d, s)
+{
+	if(t < d/2) return easeOutBack(t*2, b, c/2, d, s);
+	return easeInBack((t*2)-d, b+c/2, c/2, d, s);
+}    
+function easeInBounce(t, b, c, d)
+{
+	return c - easeOutBounce(d-t, 0, c, d) + b;
+}    
+function easeOutBounce(t, b, c, d)
+{
+	if((t/=d) <(1/2.75)) {
+		return c*(7.5625*t*t) + b;
+	} else if(t <(2/2.75)) {
+		return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+	} else if(t <(2.5/2.75)) {
+		return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+	} else {
+		return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+	}
+}    
+function easeInOutBounce(t, b, c, d)
+{
+	if(t < d/2) return easeInBounce(t*2, 0, c, d) * .5 + b;
+	else return easeOutBounce(t*2-d, 0, c, d) * .5 + c*.5 + b;
+}    
+function easeOutInBounce(t, b, c, d)
+{
+	if(t < d/2) return easeOutBounce(t*2, b, c/2, d);
+	return easeInBounce((t*2)-d, b+c/2, c/2, d);
+}
