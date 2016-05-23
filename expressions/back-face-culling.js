545240437ab3da4429fe88c0d4d714b88a003910
Namide <damien.git@gmail.com>
2016-05-23 09:39:02 +0200

    Add back-face culling

---
+++ expressions/back-face-culling.js
@@ -0,0 +1,10 @@
+Adobe After Effects 8.0 Keyframe Data
+
+Transform	Opacity
+Expression Data
+// back-face culling
+if (toCompVec([0, 0, 1])[2] > 0 ) value else 0;
+End of Expression Data
+
+
+End of Keyframe Data
